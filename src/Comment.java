13867603fb0740fb80f8922df5b637c27cd39cee
tmy <18810721315@163.com>
2020-08-01 15:26:25 +0800

    javaSE Comment

---
+++ src/Comment.java
@@ -0,0 +1,19 @@
+public class Comment {
+    /**
+     * Comment(注释)
+     *  Line Comment(单行注释)
+     *  Block Comment(多行注释)
+     *  JavaDoc(文档注释)
+     */
+    public static void main(String[] args) {
+
+        // 单行注释：Line Comment
+        /*
+           多行注释：Block Comment
+         */
+        /**
+         * 文档注释：JavaDoc
+         */
+        System.out.println("Hello,World!");
+    }
+}
